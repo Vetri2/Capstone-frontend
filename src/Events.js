5035cb013b1450b2c10b419fc4ba11bb22222db1
Vetri2 <107339279+Vetri2@users.noreply.github.com>
2023-06-29 15:09:35 +0530

    Update Events.js

--- src/Events.js
+++ src/Events.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import API_ENDPOINT from "./constant";
+import {API_ENDPOINT} from "./constant";
 
 function Events() {
     const [events, setEvents] = useState([]);
